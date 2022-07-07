d0ba340f81d1f891bb0924a678ffd59b3c69c9de
Cole Simpson <cole22simpson@gmail.com>
2022-07-06 20:33:43 -0700

    ahhh

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,7 +18,6 @@ const FILES_TO_CACHE = [
 const APP_PREFIX = 'Moolah-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
-const DATA_CACHE_NAME = "data-cache-" + VERSION;
 
 self.addEventListener('install', function (e) {
   e.waitUntil(
@@ -49,40 +48,18 @@ self.addEventListener('activate', function (e) {
   );
 });
 
-self.addEventListener("fetch", function(event) {
-  // cache all get requests to /api routes
-  if (event.request.url.includes("/api/")) {
-    event.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
-        return fetch(event.request)
-          .then(response => {
-            // If the response was good, clone it and store it in the cache.
-            if (response.status === 200) {
-              cache.put(event.request.url, response.clone());
-            }
-
-            return response;
-          })
-          .catch(err => {
-            // Network request failed, try to get it from the cache.
-            return cache.match(event.request);
-          });
-      }).catch(err => console.log(err))
-    );
-
-    return;
-  }
-
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get("accept").includes("text/html")) {
-          // return the cached home page for all requests for html pages
-          return caches.match("/");
-        }
-      });
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
     })
-  );
-});
+  )
+})
