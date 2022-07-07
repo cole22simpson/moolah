1e61a1f167910e37b75785f75db7a995efbef5c6
Cole Simpson <cole22simpson@gmail.com>
2022-07-07 01:58:23 -0700

    fixed some things

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,8 +2,8 @@ const FILES_TO_CACHE = [
   "/",
   "./index.html",
   "./css/styles.css",
-  "./js/index.js",
   "./js/idb.js",
+  "./js/index.js",
   "./manifest.json",
   "./icons/icon-72x72.png",
   "./icons/icon-96x96.png",
@@ -15,51 +15,69 @@ const FILES_TO_CACHE = [
   "./icons/icon-512x512.png"
 ];
 
-const APP_PREFIX = 'Moolah-';
+const APP_PREFIX = 'SmartSpendr-';     
 const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
+const CACHE_NAME = APP_PREFIX + VERSION
+const DATA_CACHE_NAME = "data-cache-" + VERSION;
 
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (caches) {
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
       console.log('installing cache : ' + CACHE_NAME)
-      return caches.addAll(FILES_TO_CACHE)
-    })
-  );
-})
-
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      let cacheKeepList = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      });
-      cacheKeepList.push(CACHE_NAME);
-
-      return Promise.all(
-        keyList.map(function(key, i) {
-          if (cacheKeepList.indexOf(key) === -1) {
-            console.log('deleting cache : ' + keyList[i]);
-            return caches.delete(keyList[i]);
-          }
-        })
-      );
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
+// Respond with cached resources
+self.addEventListener("fetch", function(event) {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
+        }
+      });
     })
-  )
-})
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      })
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheKeeplist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i]);
+        }
+      }));
+    })
+  );
+});
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/moolah";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/moolah";
 
 const app = express();
 
@@ -16,9 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/budget', {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 // routes
