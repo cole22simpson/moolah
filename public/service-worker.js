67a7f331043562b6315868427db68a503ad90198
Cole Simpson <cole22simpson@gmail.com>
2022-07-06 19:26:26 -0700

    added images to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,19 @@
 const FILES_TO_CACHE = [
+  "/",
   "./index.html",
   "./css/styles.css",
-  "./js/index.js"
-]
+  "./js/index.js",
+  "./js/idb.js",
+  "./manifest.json",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png"
+];
 
 const APP_PREFIX = 'Moolah-';
 const VERSION = 'version_01';
@@ -14,7 +25,7 @@ self.addEventListener('install', function (e) {
       console.log('installing cache : ' + CACHE_NAME)
       return caches.addAll(FILES_TO_CACHE)
     })
-  )
+  );
 })
 
 self.addEventListener('activate', function (e) {
@@ -39,6 +50,7 @@ self.addEventListener('activate', function (e) {
 
 self.addEventListener('fetch', function (e) {
   console.log('fetch request : ' + e.request.url)
+  
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (request) {
